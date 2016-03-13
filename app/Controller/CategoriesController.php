305347cf5515918fe8cf9e5bb83364d2f0cf2bd2
Sebastian Cytawa <skobry123on@gmail.com>
2016-03-13 23:33:21 +0100

    Pow

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -22,7 +22,7 @@ class CategoriesController extends AppController {
  */
 	public function index() {
 		$this->Category->recursive = 0;
-		$this->set('categories', $this->Paginator->paginate());//Koment
+		$this->set('categories', $this->Paginator->paginate());//Koments
 		return $this->Category->find('all');
 	}
 
